bd1b5e318bd968d0c9a8d8689c065d66df61c154
andrey-aka-skif <2119163@gmail.com>
2025-12-19 03:16:50 +0300

    style: применено форматирование по правилам prettier
    https://github.com/nii-energomash/metrology-formatting/issues/10

--- src/BaseNumericFormattedValue.js
+++ src/BaseNumericFormattedValue.js
@@ -2,42 +2,43 @@
  * Базовый класс форматированных числовых значений
  */
 export default class BaseNumericFormattedValue {
-    rawValue = NaN
-    numericValue = NaN
+  rawValue = NaN
+  numericValue = NaN
 
-    _isCalculated = false
+  _isCalculated = false
 
-    constructor(value) {
-        if (new.target === BaseNumericFormattedValue)
-            throw new Error(`${new.target.name} нельзя инстанцировать напрямую`);
+  constructor(value) {
+    if (new.target === BaseNumericFormattedValue)
+      throw new Error(`${new.target.name} нельзя инстанцировать напрямую`)
 
-        this.rawValue = value
+    this.rawValue = value
+  }
+
+  _ensureCalculateOrThrow() {
+    if (!this._isCalculated) {
+      this.numericValue = this._getValidNumericValueOrThrow()
+      this._calculateOrThrow()
+      this._isCalculated = true
     }
+  }
 
-    _ensureCalculateOrThrow() {
-        if (!this._isCalculated) {
-            this.numericValue = this._getValidNumericValueOrThrow()
-            this._calculateOrThrow()
-            this._isCalculated = true
-        }
-    }
+  _getValidNumericValueOrThrow() {
+    if (!isFinite(this.rawValue))
+      throw new Error('Значение не может быть представлено числом')
 
-    _getValidNumericValueOrThrow() {
-        if (!isFinite(this.rawValue))
-            throw new Error("Значение не может быть представлено числом")
+    const numericValue = parseFloat(this.rawValue)
 
-        const numericValue = parseFloat(this.rawValue)
+    if (isNaN(numericValue)) throw new Error('Значение является NaN')
 
-        if (isNaN(numericValue))
-            throw new Error("Значение является NaN")
+    return numericValue
+  }
 
-        return numericValue
-    }
+  _roundTo(value, decimalPlaces) {
+    const multiplier = Math.pow(10, decimalPlaces)
+    return Math.round(value * multiplier) / multiplier
+  }
 
-    _roundTo(value, decimalPlaces) {
-        const multiplier = Math.pow(10, decimalPlaces)
-        return Math.round(value * multiplier) / multiplier
-    }
-
-    _calculateOrThrow() { throw new Error('Метод не переопределён') }
+  _calculateOrThrow() {
+    throw new Error('Метод не переопределён')
+  }
 }
